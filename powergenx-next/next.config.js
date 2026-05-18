358a23d240a1816669fa29e61929d146014b3e93
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2026-05-18 16:20:40 +0200

    stuff

--- powergenx-next/next.config.js
+++ powergenx-next/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   output: 'export',
-  basePath: process.env.BASE_PATH || '',
+  distDir: '../compilation',
+
+  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: "/powergenx-next/compilation",
+  assetPrefix: "/powergenx-next/compilation",
 }
 
 module.exports = nextConfig
